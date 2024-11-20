de97246f16706081119569ae9ca9ad4ffd2ef4e7
Vittorio Kevin <volansedev@gmail.com>
2024-11-20 20:34:43 -0300

    update
    adiciona pagina sandbox do bootstrap

--- study-flow/pages/home/script.js
+++
@@ -1,9 +0,0 @@
-//quando a DOM for carregada esse script busca e cria o component navbar
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("../../components/navbar/index.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container").innerHTML = data;
-        })
-        .catch(error => console.error("Erro ao carregar navbar:", error));
-})
--- study-flow/pages/tasks/script.js
+++
@@ -1,9 +0,0 @@
-//Criação e importação do navbar
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("../../components/navbar/index.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container").innerHTML = data;
-        })
-        .catch(error => console.error("Erro ao carregar navbar:", error));
-})
